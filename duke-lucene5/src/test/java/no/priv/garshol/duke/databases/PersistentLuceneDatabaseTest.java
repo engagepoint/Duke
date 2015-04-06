ede4c0015758e40f74305b1f7d6772494568cf66
Mykhailo Gorbenko <mykhailo.gorbenko@engagepoint.com>
2015-04-06 12:13:25 +0300

    Added lucene 5.0.0

---
+++ duke-lucene5/src/test/java/no/priv/garshol/duke/databases/PersistentLuceneDatabaseTest.java
@@ -0,0 +1,16 @@
+
+package no.priv.garshol.duke.databases;
+
+import no.priv.garshol.duke.Configuration;
+import no.priv.garshol.duke.Database;
+
+public class PersistentLuceneDatabaseTest extends PersistentDatabaseTest {
+
+    public Database createDatabase(Configuration config) {
+        LuceneDatabase db = new LuceneDatabase();
+        db.setOverwrite(false);
+        db.setConfiguration(config);
+        db.setPath(tmpdir.getRoot().getAbsolutePath());
+        return db;
+    }
+}
